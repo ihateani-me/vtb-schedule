f3cbaf7f183b11418c3cab2254cbd92415e3023a
NoAiOne <aimanmh@my.smccd.edu>
2020-05-21 15:41:35 +0700

    web(routes/hololive): replace `/upcoming` route with `/live` route

--- web/routes/hololive.py
+++ web/routes/hololive.py
@@ -8,6 +8,7 @@ from utils.dbconn import (
     fetch_channels,
     fetch_data,
     fetch_holobili,
+    fetch_hlive_data,
     parse_uuids_args,
 )
 from utils.models import BiliChannelsModel, BiliScheduleModel
@@ -16,21 +17,22 @@ holobp = Blueprint("Hololive", "/", strict_slashes=True)
 
 
 @holobp.get("/upcoming")
-@doc.summary("Upcoming HoloLive streams")
+@doc.summary("Live/Upcoming HoloLive streams")
 @doc.description(
-    "Fetch a list of upcoming streams from HoloLive VTubers"
-    ", updated every 4 minutes via cronjob."
+    "Fetch a list of live/upcoming streams from HoloLive VTubers"
+    ", updated every 2/4 minutes via cronjob."
 )
 @doc.consumes(
     doc.String(
         name="uids",
-        description="Filter results with User ID "
+        description="Filter upcoming results with User ID "
         "(support multiple id separated by comma)",
     ),
     location="query",
 )
 @doc.produces(
     {
+        "live": doc.List(BiliScheduleModel),
         "upcoming": doc.List(BiliScheduleModel),
         "cached": doc.Boolean(
             "Is the results cached or not?", True, "cached", choices=[True]
@@ -39,11 +41,17 @@ holobp = Blueprint("Hololive", "/", strict_slashes=True)
     description="A list of upcoming streams",
     content_type="application/json",
 )
-async def holoupcoming_api(request):
+async def hololiveup_api(request):
     logger.info(f"Requested {request.path} data")
     upcoming_results = await fetch_data("holobili", fetch_holobili)
+    upcoming_results = await parse_uuids_args(request.args, upcoming_results)
+    lives_results = await fetch_data("liveholo", fetch_hlive_data)
     return json(
-        await parse_uuids_args(request.args, upcoming_results),
+        {
+            "live": lives_results["live"],
+            "upcoming": upcoming_results["upcoming"],
+            "cached": True,
+        },
         dumps=ujson.dumps,
         ensure_ascii=False,
     )
