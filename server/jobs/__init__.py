53592252e463405ec90adda9f1b0f13288a9d506
NoAiOne <aimanmh@my.smccd.edu>
2020-06-01 14:01:40 +0700

    server(jobs/youtube_others): create youtube_channels for channels stats

--- server/jobs/__init__.py
+++ server/jobs/__init__.py
@@ -5,4 +5,10 @@ from .channels_bili import update_channels_stats
 from .hololive import hololive_main
 from .nijisanji import nijisanji_main
 from .others import others_main
-from .youtube_others import youtube_live_heartbeat, youtube_video_feeds
+from .twitcasting import twitcasting_channels, twitcasting_heartbeat
+from .twitch import twitch_channels, twitch_heartbeat
+from .youtube_others import (
+    youtube_channels,
+    youtube_live_heartbeat,
+    youtube_video_feeds,
+)
--- server/jobs/youtube_others.py
+++ server/jobs/youtube_others.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import re
 from datetime import datetime, timezone
 
 import aiohttp
@@ -22,10 +21,10 @@ async def fetch_xmls(
 
 
 async def fetch_apis(
-    session: aiohttp.ClientSession, param: dict, channel: str
-) -> dict:
+    session: aiohttp.ClientSession, endpoint: str, param: dict, channel: str
+) -> tuple:
     async with session.get(
-        "https://www.googleapis.com/youtube/v3/videos", params=param
+        f"https://www.googleapis.com/youtube/v3/{endpoint}", params=param
     ) as res:
         items_data = await res.json()
     return items_data, channel
@@ -35,8 +34,7 @@ async def youtube_video_feeds(
     DatabaseConn: VTBiliDatabase, dataset: str, yt_api_key: str
 ):
     vtlog = logging.getLogger("yt_videos_feeds")
-    sessions = aiohttp.ClientSession(headers={"User-Agent": "VTHellAPI/0.4.0"})
-    ytid_re = re.compile(r"yt\:video\:(?P<ids>.*)")
+    sessions = aiohttp.ClientSession(headers={"User-Agent": "VTHellAPI/0.6.0"})
 
     vtlog.debug("Opening dataset")
     with open(dataset, "r", encoding="utf-8") as fp:
@@ -44,7 +42,6 @@ async def youtube_video_feeds(
 
     vtlog.info("Fetching all fetched video IDs...")
     fetched_video_ids = await DatabaseConn.fetch_data("yt_other_videoids")
-    fetched_video_ids: list = fetched_video_ids["ids"]
     youtube_lives_data = await DatabaseConn.fetch_data("yt_other_livedata")
     del youtube_lives_data["_id"]
 
@@ -58,15 +55,25 @@ async def youtube_video_feeds(
     for xmls in asyncio.as_completed(xmls_to_fetch):
         feed_results, channel, nn = await xmls
 
-        vtlog.info(f"Processing XMLs: {channels_dataset[nn]['name']}")
+        fetched_videos = []
+        if channel in fetched_video_ids:
+            fetched_videos = fetched_video_ids[channel]
+
+        vtlog.info(f"|=> Processing XMLs: {channels_dataset[nn]['name']}")
         video_ids = []
         for entry in feed_results.entries:
-            match_id = re.search(ytid_re, entry["id"])
-            ids_ = match_id.group("ids")
-            if ids_ not in fetched_video_ids:
+            ids_ = entry["yt_videoid"]
+            if ids_ not in fetched_videos:
                 video_ids.append(ids_)
+                fetched_videos.append(ids_)
 
         collected_videos_ids[channel] = video_ids
+        vtlog.info(
+            f"|=> Updating fetched IDs: {channels_dataset[nn]['name']}.."
+        )
+        await DatabaseConn.update_data(
+            "yt_other_videoids", {channel: fetched_videos}
+        )
 
     vtlog.info("Collected!")
     vtlog.info("Now creating tasks for a non-fetched Video IDs to the API.")
@@ -82,7 +89,7 @@ async def youtube_video_feeds(
             "key": yt_api_key,
         }
         vtlog.info(f"|-- Processing: {chan}")
-        video_to_fetch.append(fetch_apis(sessions, param, chan))
+        video_to_fetch.append(fetch_apis(sessions, "videos", param, chan))
 
     if not video_to_fetch:
         vtlog.warn("|== No video to fetch, bailing!")
@@ -98,7 +105,7 @@ async def youtube_video_feeds(
         youtube_videos_data = youtube_lives_data[ch_id]
         for res_item in video_results["items"]:
             video_id = res_item["id"]
-            fetched_video_ids.append(video_id)
+            # fetched_video_ids.append(video_id)
             if "liveStreamingDetails" not in res_item:
                 continue
             snippets = res_item["snippet"]
@@ -144,9 +151,7 @@ async def youtube_video_feeds(
         upd_data = {ch_id: youtube_videos_data}
         await DatabaseConn.update_data("yt_other_livedata", upd_data)
 
-    vtlog.info("Updating fetched IDs database..")
-    upd_data = {"ids": fetched_video_ids}
-    await DatabaseConn.update_data("yt_other_videoids", upd_data)
+    vtlog.info("Closing sessions...")
     await sessions.close()
 
 
@@ -154,7 +159,7 @@ async def youtube_live_heartbeat(
     DatabaseConn: VTBiliDatabase, yt_api_key: str
 ):
     vtlog = logging.getLogger("yt_live_heartbeat")
-    session = aiohttp.ClientSession(headers={"User-Agent": "VTHellAPI/0.5.0"})
+    session = aiohttp.ClientSession(headers={"User-Agent": "VTHellAPI/0.6.0"})
 
     vtlog.info("Fetching live data...")
 
@@ -176,7 +181,7 @@ async def youtube_live_heartbeat(
         "id": ",".join(videos_list),
         "key": yt_api_key,
     }
-    items_data, _ = await fetch_apis(session, param, "nullify")
+    items_data, _ = await fetch_apis(session, "videos", param, "nullify")
     await session.close()
 
     vtlog.info(f"Parsing results...")
@@ -219,3 +224,94 @@ async def youtube_live_heartbeat(
 
     vtlog.info("|-- Updating database...")
     await DatabaseConn.update_data("yt_other_livedata", youtube_lives_data)
+
+
+async def youtube_channels(
+    DatabaseConn: VTBiliDatabase, dataset: str, yt_api_key: str
+):
+    vtlog = logging.getLogger("yt_channels")
+    sessions = aiohttp.ClientSession(headers={"User-Agent": "VTHellAPI/0.6.0"})
+
+    vtlog.debug("Opening dataset")
+    with open(dataset, "r", encoding="utf-8") as fp:
+        channels_dataset = ujson.load(fp)
+
+    vtlog.info("Creating task for channels data.")
+    channels_tasks = []
+    for channel in channels_dataset:
+        vtlog.info(f"|-> Adding {channel['name']}")
+        param = {
+            "part": "snippet,statistics",
+            "id": channel["id"],
+            "key": yt_api_key,
+        }
+        channels_tasks.append(
+            fetch_apis(sessions, "channels", param, channel["name"])
+        )
+
+    vtlog.info("Running all tasks...")
+    final_channels_dataset = []
+    for chan_task in asyncio.as_completed(channels_tasks):
+        chan_data, chan_name = await chan_task
+        vtlog.info(f"|--> Processing: {chan_name}")
+
+        if "items" not in chan_data:
+            vtlog.warn(f"|--! Failed to fetch: {chan_name}")
+            continue
+        chan_data = chan_data["items"]
+        if not chan_data:
+            vtlog.warn(f"|--! Empty data on {chan_name}")
+            continue
+        chan_data = chan_data[0]
+        if not chan_data:
+            vtlog.warn(f"|--! Empty data on {chan_name}")
+            continue
+
+        chan_snip = chan_data["snippet"]
+        chan_stats = chan_data["statistics"]
+
+        ch_id = chan_data["id"]
+        title = chan_snip["title"]
+        desc = chan_snip["description"]
+        pubat = chan_snip["publishedAt"]
+
+        thumbs_data = chan_snip["thumbnails"]
+        if "high" in thumbs_data:
+            thumbs = thumbs_data["high"]["url"]
+        elif "medium" in thumbs_data:
+            thumbs = thumbs_data["medium"]["url"]
+        else:
+            thumbs = thumbs_data["default"]["url"]
+
+        subscount = chan_stats["subscriberCount"]
+        viewcount = chan_stats["viewCount"]
+        vidcount = chan_stats["videoCount"]
+
+        try:
+            subscount = int(subscount)
+            viewcount = int(viewcount)
+            vidcount = int(vidcount)
+        except ValueError:
+            pass
+
+        data = {
+            "id": ch_id,
+            "name": title,
+            "description": desc,
+            "publishedAt": pubat,
+            "thumbnail": thumbs,
+            "subscriberCount": subscount,
+            "viewCount": viewcount,
+            "videoCount": vidcount,
+        }
+
+        final_channels_dataset.append(data)
+
+    vtlog.info("Final sorting...")
+    if final_channels_dataset:
+        final_channels_dataset.sort(key=lambda x: x["name"])
+
+    vtlog.info("Updating database...")
+    upd_data = {"channels": final_channels_dataset}
+    await DatabaseConn.update_data("yt_other_channels", upd_data)
+    await sessions.close()
